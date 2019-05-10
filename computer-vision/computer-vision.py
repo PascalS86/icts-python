ff77d647f832040ef5995764e869a8d37a386e9c
Pascal Senjic <pascal.senjic@gmail.com>
2019-05-10 09:41:30 +0200

    merged

--- computer-vision/computer-vision.py
+++ computer-vision/computer-vision.py
@@ -3,6 +3,12 @@ import datetime
 import cv2
 
 
+#http://benalexkeen.com/using-azure-cognitive-services-to-caption-video/
+#88726c45c82248f3816bcc223ea56ab8
+#https://mosellandwerkstaetten.de/shop/media/image/4d/68/c9/untersetzer-tasse-loeffel.jpg
+
+#https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
+
 subscription_key = "" # your subscription key here
 vision_tag_url = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/tag"
 
