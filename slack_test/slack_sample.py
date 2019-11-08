e604219415094d2f2412b52787889d3f4b531f04
Pascal Senjic <pascal.senjic@gmail.com>
2019-11-08 14:43:11 +0100

    update slack

---
+++ slack_test/slack_sample.py
@@ -0,0 +1,24 @@
+
+slack_token = "[API_TOKEN]"
+import os
+import slack
+
+@slack.RTMClient.run_on(event='message')
+def say_hello(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    print(data)
+    # if 'Hello' in data.get('text', []):
+    #     channel_id = data['channel']
+    #     thread_ts = data['ts']
+    #     user = data['user']
+    #     # web_client.chat_postMessage(
+    #     #     channel=channel_id,
+    #     #     text=f"Hi <@{user}>!",
+    #     #     thread_ts=thread_ts
+    #     # )
+
+slack_token = slack_token
+rtm_client = slack.RTMClient(token=slack_token)
+rtm_client.start()
